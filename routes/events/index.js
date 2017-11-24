60d124397f028f28d1bdf8dc797b70ca89d22741
Piotrek <piotr.pietruszka@o2.pl>
2017-11-25 00:57:27 +0100

    implement event module

--- routes/events/index.js
+++ routes/events/index.js
@@ -11,11 +11,12 @@ class EventRouter{
   };
 
   init(){
-    this.eventRouter.get('/test', (req, res) => {
+    this.eventRouter.post('/events', (req, res) => {
+      if(!(req.body.page && req.body.limit, req.body.query && req.body.filters)) rews.status(400).json({success: false, message: "Missing parameters."});
       Event.getEvent(1).then(result => {
-        res.json(result)
+        res.status(200).json({success: true, data: result})
       }).catch(err => {
-        res.json({err: err});
+        res.status(400).json({success: false, message: "Getting events error."});
       });
     });
 
@@ -29,14 +30,21 @@ class EventRouter{
         accessToken: "520060218367018|Wf6VqXyBStK4qEajr_QDY6wq6Uw",
         "lat": 50.7669686,
         "lng": 16.2765734,
-        distance: 10000
+        distance: 10000,
+        sort: 'data'
       }).then(function (events) {
 
         let _mock = events.events.map(element => {
-          return {name: element.name, profileImg: element.profilePicture,
-            coverImg: element.coverPicture, description: element.description,
-            startTime: element.startTime, endTime:element.endTime,
-            category: element.category, place: {
+          console.log(element.place.location,111111)
+          return {
+            name: element.name,
+            profileImg: element.profilePicture,
+            coverImg: element.coverPicture,
+            description: element.description,
+            startTime: element.startTime,
+            endTime:element.endTime,
+            category: element.category,
+            place: {
               name: element.place.location.name,
               lat: element.place.location.latitude,
               lng: element.place.location.longitude,
@@ -55,13 +63,19 @@ class EventRouter{
       }).catch(function (error) {
         console.error(JSON.stringify(error));
       });
+    });
 
+    this.eventRouter.post('/score', (req, res) => {
+      if(req.body.id) res.status(400).json({success: false, message: "Missing parameter."})
+      Event.findByIdAndUpdate(req.body.id, {$inc: {score: 1}}, err =>{
+        if(!err)res.status(200).json({success: true});
+        else res.status(400).json({success: false, message: "Adding score error."})
+      });
+    });
 
-      //
-      // new Event({name: 'aaaa'}).save(err=> {
-      //   if(err) res.json(err);
-      //   else res.json({success: true})
-      // });
+    this.eventRouter.get('/markers', (req,res) => {
+      Event.getMarkers().then(results => res.json({success: true, data: results}))
+        .catch(err => res.json({success:false, message: "Markers get error."}))
     });
 
 
