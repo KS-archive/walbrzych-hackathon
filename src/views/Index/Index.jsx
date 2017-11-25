fb57071801a4564af2402746955eefa9a0f80f4a
Konrad Szwarc <konrad.szwarc@outlook.com>
2017-11-25 16:30:07 +0100

    Preloader

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -3,6 +3,16 @@ import Nav from '../../components/Nav/Nav';
 import { Container, Body } from './Index_styles';
 
 export default class Index extends Component {
+  componentDidMount() {
+    const preloader = document.getElementById('preloader');
+    console.log(preloader);
+    preloader.setAttribute('class', 'preload hidden');
+    console.log(preloader);
+    setTimeout(() => {
+      preloader.setAttribute('class', 'preload removed');
+    }, 1000);
+  }
+
   render() {
     return (
       <Container>
